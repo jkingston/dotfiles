971672e716adc72cdf98c751ba7133a1c2e1e4d0
Jack Kingston <j.kngstn@gmail.com>
2026-06-01 17:29:56 +0100

    Restore GTK rbw pinentry

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1095,8 +1095,7 @@ test_flatpak_package_contract() {
 
 test_rbw_package_contract() {
   assert_repo_contains package-sets.sh 'github-cli direnv mise lazygit lazydocker openssh rbw rofi-rbw wtype gum' &&
-    assert_repo_contains package-sets.sh 'kguiaddons kwindowsystem' &&
-    assert_repo_contains DESKTOPS.md 'pacman -Q openssh rbw rofi-rbw wtype gum kguiaddons kwindowsystem' &&
+    assert_repo_contains DESKTOPS.md 'pacman -Q openssh rbw rofi-rbw wtype gum' &&
     assert_repo_contains dot_bashrc 'SSH_AUTH_SOCK="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}/rbw/ssh-agent-socket"' &&
     assert_repo_contains dot_config/environment.d/rbw-ssh-agent.conf 'SSH_AUTH_SOCK=${XDG_RUNTIME_DIR}/rbw/ssh-agent-socket' &&
     assert_repo_contains dot_config/systemd/user/rbw-agent.service 'ExecStart=/usr/bin/rbw-agent --no-daemonize' &&
@@ -1104,8 +1103,7 @@ test_rbw_package_contract() {
     assert_repo_contains dot_local/bin/executable_rbw-menu 'rofi-rbw' &&
     assert_repo_contains dot_local/bin/executable_rbw-menu '--typer wtype' &&
     assert_repo_contains dot_local/bin/executable_rbw-menu 'rbw config set pinentry "$HOME/.local/bin/rbw-pinentry"' &&
-    assert_repo_contains dot_local/bin/executable_rbw-pinentry.tmpl 'QT_SCALE_FACTOR="${PINENTRY_QT_SCALE_FACTOR:-{{ .monitor_scale }}}"' &&
-    assert_repo_contains dot_local/bin/executable_rbw-pinentry.tmpl 'exec /usr/bin/pinentry-qt "$@"' &&
+    assert_repo_contains dot_local/bin/executable_rbw-pinentry.tmpl 'GDK_SCALE="${PINENTRY_GDK_SCALE:-{{ if ge .monitor_scale "1.5" }}2{{ else }}1{{ end }}}"' &&
     assert_repo_contains dot_local/bin/executable_rbw-menu 'Missing $command; install rbw and rofi-rbw' &&
     assert_repo_contains dot_local/bin/executable_rbw-menu 'Set server (current: %s)' &&
     assert_repo_contains dot_local/bin/executable_rbw-menu 'rbw config set base_url "$url"' &&
